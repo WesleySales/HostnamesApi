9d71754f49421f6d29b43de7d5746d1cdb80c8ec
WesleySales <WesleySales.vb@gmail.com>
2025-12-20 08:24:27 -0300

    feat: adicionei a referencia aos repositories dos dois bancos associados ao projeto

--- src/main/java/com/wesley/ValidadorHostnames/ValidadorHostnamesApplication.java
+++ src/main/java/com/wesley/ValidadorHostnames/ValidadorHostnamesApplication.java
@@ -1,18 +1,25 @@
 package com.wesley.ValidadorHostnames;
 
-import com.wesley.ValidadorHostnames.services.WhitelistService;
+//import com.wesley.ValidadorHostnames.services.Postgre.WhitelistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableCaching
+
+// Define onde estão os repositórios JPA (SQL)
+@EnableJpaRepositories(basePackages = "com.wesley.ValidadorHostnames.repositories.Postgre")
+// Define onde estão os repositórios MongoDB (NoSQL)
+@EnableMongoRepositories(basePackages = "com.wesley.ValidadorHostnames.repositories.MongoDB")
 public class ValidadorHostnamesApplication implements CommandLineRunner {
 
 	@Autowired
-	private WhitelistService whitelistService;
+//	private WhitelistService whitelistService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ValidadorHostnamesApplication.class, args);
